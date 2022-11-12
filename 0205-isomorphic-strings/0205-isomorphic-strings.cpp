9ab213854b2f4875484f9cec0d75e9a145f822e4
aarish21 <aarish.jerry@gmail.com>
2022-11-13 01:02:26 +0530

    Time: 15 ms (52.94%), Space: 6.9 MB (73.63%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -2,7 +2,7 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        map<char,char> m1,m2;
+        unordered_map<char,char> m1,m2;
         
         for(int i=0;i<s.length();i++){
             
