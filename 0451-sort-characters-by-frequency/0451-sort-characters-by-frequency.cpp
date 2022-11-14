94d4dc24bc56fc2d29d71247ff5b087aa5ea0278
aarish21 <aarish.jerry@gmail.com>
2022-11-14 22:07:08 +0530

    Time: 27 ms (50.85%), Space: 8.3 MB (77.14%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -1,16 +1,33 @@
 class Solution {
 public:
+//     string frequencySort(string s) {
+//        vector<pair<int,char>> map('z'+1,{0,0});
+//         string res="";
+//         for(char a: s)
+//             map[a] = {map[a].first+1,a};
+        
+//         sort(map.begin(),map.end());
+        
+//         for(auto p: map)
+//             res = string(p.first,p.second) + res;
+        
+//         return res;
+//     }
     string frequencySort(string s) {
-       vector<pair<int,char>> map('z'+1,{0,0});
-        string res="";
-        for(char a: s)
-            map[a] = {map[a].first+1,a};
-        
-        sort(map.begin(),map.end());
-        
-        for(auto p: map)
-            res = string(p.first,p.second) + res;
-        
-        return res;
+        unordered_map<char,int> freq;           //for frequency of characters
+        priority_queue<pair<int,char>> maxheap; 
+        //maxheap according to frequency of characters
+        for(char c: s)
+            freq[c]++;
+        for(auto it: freq)
+            maxheap.push({it.second,it.first}); 
+        //heap will be constructed on the basis of frequency
+        s="";   
+        while(!maxheap.empty()){
+            s+=string(maxheap.top().first,maxheap.top().second); 
+            //frequency times the character
+            maxheap.pop();
+        }
+        return s;
     }
 };
