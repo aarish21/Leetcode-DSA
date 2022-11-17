b94e7e7e4c5a317104a0bd00d7076460007fe5e8
aarish21 <aarish.jerry@gmail.com>
2022-11-18 01:27:47 +0530

    Time: 7 ms (52.42%), Space: 7.1 MB (12.30%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+    int myAtoi(string s) {
+        bool isNeg = false;
+        int i=0;
+        long long ans = 0;
+        
+        while(i < s.size()) {
+           if(s[i]==' ') i++;
+            else break;
+        }
+        if(s[i] == '-'){
+            isNeg = true;
+            i++;
+        }else if(s[i] == '+') i++;
+        
+            
+        
+         for(int j=i; j < s.size(); j++) {
+            if(s[j] >= '0' and s[j] <= '9') {
+                ans = ans * 10 + (s[j] - '0');
+                if(ans >= INT_MAX) break;   
+            }
+            else break;
+        }
+        if(isNeg)
+            ans= ans*-1;
+        if(ans <= INT_MIN) return INT_MIN;
+        else if(ans >= INT_MAX) return INT_MAX;
+        return ans;
+    }
+//     int myAtoi(string s) {
+//         int i = 0, flag = 0;
+//         while(i < s.size()) {
+//             if(s[i] == ' ') i++;   
+//             else break;       
+//         }
+//         if(s[i] == '-') {
+//             flag = 1;
+//             i++;
+//         }
+//         else if(s[i] == '+') i++;
+
+//         long long num = 0;
+//         for(int j=i; j<s.size(); j++) {
+//             if(s[j] >= '0' and s[j] <= '9') {
+//                 num = num * 10 + (s[j] - '0');
+//                 if(num >= INT_MAX) break;   
+//             }
+//             else break;
+//         }
+        
+//         if(flag) num *= -1;
+//         if(num <= INT_MIN) return INT_MIN;
+//         else if(num >= INT_MAX) return INT_MAX;
+//         return num;
+//     }
+    
+};
