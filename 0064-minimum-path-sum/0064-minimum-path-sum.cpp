6047fa15b73b9b5e2ade8005bde286fcf6547aa7
aarish21 <aarish.jerry@gmail.com>
2023-01-18 15:02:30 +0530

    Time: 16 ms (41.46%), Space: 9.9 MB (66.88%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,30 +1,32 @@
 class Solution {
 public:
-    int uniquePaths(int m, int n,vector<vector<int>> &dp,vector<vector<int>> &grid) {
+    int uniquePaths(int m, int n,vector<int> &dp,vector<vector<int>> &grid) {
         
     for(int i=0;i<m;i++){
+        vector<int> temp(n,0);
         for(int j=0;j<n;j++){
-            if(i==0 && j == 0) dp[i][j]=grid[i][j];
+            if(i==0 && j == 0) temp[j]=grid[i][j];
             else{
                 int up=grid[i][j];
                 int left = grid[i][j];
-                if(i>0) up += dp[i-1][j];
+                if(i>0) up += dp[j];
                 else up+= 1e9;
-                if(j>0) left += dp[i][j-1];
+                if(j>0) left += temp[j-1];
                 else left+= 1e9;
 
-                dp[i][j] = min(up,left);
+                temp[j] = min(up,left);
             }
         }
+        dp = temp;
         
     }
 
-        return dp[m-1][n-1];
+        return dp[n-1];
     }
     int minPathSum(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-        vector<vector<int>> dp(m,vector<int>(n,0));
+        vector<int> dp(n,0);
         return uniquePaths(m,n,dp,grid);
     }
 };
