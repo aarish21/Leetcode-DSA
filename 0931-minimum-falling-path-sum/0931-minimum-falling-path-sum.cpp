937470457056bb74833590043adc95e5e73bc7ef
aarish21 <aarish.jerry@gmail.com>
2023-01-20 02:06:34 +0530

    Time: 16 ms (79.83%), Space: 10.4 MB (20.31%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -1,20 +1,26 @@
 class Solution {
 public:
     int minFallingPathSum(vector<vector<int>>& matrix) {
-        int rows = matrix.size();
-        int cols = matrix.size();
-        for(int i = 1; i < rows; i++){
-            for(int j = 0; j < cols; j++){
-                int rowUp = matrix[i-1][j];
-                int leftUp = j>0?matrix[i-1][j-1]:INT_MAX;
-                int rightUp = j<cols-1?matrix[i-1][j+1]:INT_MAX;
-                matrix[i][j] += min(rowUp, min(leftUp, rightUp));
+        int n = matrix.size();
+        vector<vector<int>> dp(n,vector<int>(n,0));
+        for(int j=0;j<n;j++) dp[n-1][j] = matrix[n-1][j];
+        for(int i=n-2;i>=0;i--){
+            for(int j=n-1;j>=0;j--){
+                int d = matrix[i][j]+dp[i+1][j];
+                int diaR = INT_MAX;
+                if(j<n-1)
+                diaR = matrix[i][j]+dp[i+1][j+1];
+                int diaL = INT_MAX;
+                if(j>0)
+                diaL = matrix[i][j]+dp[i+1][j-1];
+                dp[i][j] = min(d,min(diaL,diaR));
             }
         }
-        int answer = INT_MAX;
-        for(int j=0;j < cols; j++){
-            answer = min(answer, matrix[rows-1][j]);
+        int mini = INT_MAX;
+    
+        for(int j=0; j<n;j++){
+            mini = min(mini,dp[0][j]);
         }
-        return answer;
+        return mini;
     }
 };
