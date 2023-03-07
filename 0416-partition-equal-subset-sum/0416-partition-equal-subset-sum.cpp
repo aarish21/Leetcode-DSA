87c076ec1c6cb9e4fc1810622ad94ee77e41b66c
aarish21 <aarish.jerry@gmail.com>
2023-03-08 03:42:56 +0530

    Time: 297 ms (65.08%), Space: 12.8 MB (73.39%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,32 +1,18 @@
 class Solution {
 public:
+    // memoization
     bool subSetSum(int i,int target,vector<int>& nums,vector<vector<int>>& dp){
-//         if(target == 0) return true;
-//         if(i==0) return nums[0]==target;
-//         if(dp[i][target]!=-1) return dp[i][target];
-//         bool notTake = subSetSum(i-1,target,nums,dp);
-//         bool take = false;
-//         if(nums[i]<=target) take = subSetSum(i-1,target-nums[i],nums,dp);
+        if(target == 0) return true;
+        if(i==0) return nums[0]==target;
+        if(dp[i][target]!=-1) return dp[i][target];
+        bool notTake = subSetSum(i-1,target,nums,dp);
+        bool take = false;
+        if(nums[i]<=target) take = subSetSum(i-1,target-nums[i],nums,dp);
         
-//         return dp[i][target] = take || notTake;
-        
-        
-        for(int i=0;i<nums.size();i++) dp[i][0] = true;
-        
-        if(nums[0]<=target) dp[0][nums[0]] = true;
-        
-        for(int i=1;i<nums.size();i++){
-            for(int k = 1;k<=target;k++){
-                bool notTake = dp[i-1][target];
-                bool take = false;
-                if(nums[i]<=target) take = dp[i-1][target-nums[i]];
-        
-                dp[i][target] = take || notTake;
-            }
-        }
-        return dp[nums.size()-1][target];
+        return dp[i][target] = take || notTake;
     }
-    bool subsetSumToK(int n, int k, vector<int> &arr){
+    // tabulation
+    bool subsetSumToK1(int n, int k, vector<int> &arr){
         vector<vector<bool>> dp(n,vector<bool>(k+1,false));
 
         for(int i=0; i<n; i++){
@@ -50,6 +36,32 @@ public:
         }
 
         return dp[n-1][k];
+    }
+    // space optimization
+    bool subsetSumToK(int n, int k, vector<int> &arr){
+        vector<bool> prev(k+1,false);
+
+        prev[0] = true;
+
+        if(arr[0]<=k)
+            prev[arr[0]] = true;
+
+        for(int ind = 1; ind<n; ind++){
+            vector<bool> cur(k+1,false);
+            cur[0] = true;
+            for(int target= 1; target<=k; target++){
+                bool notTaken = prev[target];
+
+                bool taken = false;
+                    if(arr[ind]<=target)
+                        taken = prev[target-arr[ind]];
+
+                cur[target]= notTaken||taken;
+            }
+            prev = cur;
+        }
+
+        return prev[k];
     }
     bool canPartition(vector<int>& nums) {
        
