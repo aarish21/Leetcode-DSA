ade32323815e3d0ac38a5dbbf6b9963e9bafd3fa
aarish21 <aarish.jerry@gmail.com>
2023-03-11 16:11:34 +0530

    Time: 7 ms (95.85%), Space: 9.6 MB (54.78%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -23,7 +23,23 @@ public:
         if(totalSum-d<0) return 0;
         if((totalSum-d)%2==1) return 0;
         int s2 = (totalSum-d)/2;
-        vector<vector<int>> dp(n,vector<int>(s2+1,-1));
-        return helper(n-1,s2,nums,dp);
+        // vector<vector<int>> dp(n,vector<int>(s2+1,-1));
+        // return helper(n-1,s2,nums,dp);
+        vector<int> prev(s2+1,0), cur(s2+1,0);
+        if (nums[0] <= s2) prev[nums[0]] = 1;
+        prev[0] = (nums[0] == 0) ? 2 : 1;
+
+
+        for (int i = 1; i < n; i++) {
+        for (int s = 0; s <= s2; s++) {
+        int notTake = prev[s];
+        int take = 0;
+        if (nums[i] <= s) take = prev[s - nums[i]];
+        cur[s] = (take + notTake);
+        }
+        prev = cur;
+        }
+
+        return prev[s2];
     }
 };
