a2e110acb77b4254dcc3d5da77e6ecc465501444
aarish21 <aarish.jerry@gmail.com>
2023-03-30 15:03:45 +0530

    Time: 759 ms (33.12%), Space: 217.9 MB (18.65%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int helper(int ind,int buy,int count,vector<int>& prices, vector<vector<vector<int>>> &dp){
-        if(ind == prices.size() || count == 0) return 0;
+        if(ind == prices.size() || count == 2) return 0;
         int profit;
         if(dp[ind][buy][count]!=-1) return dp[ind][buy][count];
         if(buy==0){
             profit = max(0+helper(ind+1,0,count,prices,dp),-prices[ind]+helper(ind+1,1,count,prices,dp));
         } 
         if(buy == 1) {
-            profit = max(0+helper(ind+1,1,count,prices,dp),prices[ind]+helper(ind+1,0,count-1,prices,dp));
+            profit = max(0+helper(ind+1,1,count,prices,dp),prices[ind]+helper(ind+1,0,count+1,prices,dp));
         }
         
        return dp[ind][buy][count] = profit;
@@ -16,6 +16,6 @@ public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
         vector<vector<vector<int>>> dp(n,vector<vector<int>>(2,vector<int>(3,-1)));
-        return helper(0,0,2,prices,dp);
+        return helper(0,0,0,prices,dp);
     }
 };
