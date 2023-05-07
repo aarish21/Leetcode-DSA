feac082aba2b496ac6bf3b5d72701a4df124c7ad
aarish21 <aarish.jerry@gmail.com>
2023-05-08 02:06:32 +0530

    Time: 0 ms (100.00%), Space: 8.5 MB (53.02%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -1,37 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
+    void helper(TreeNode* root,vector<int> &v){
+        if(root==NULL) return;
+        v.push_back(root->val);
+        helper(root->left,v);
+        helper(root->right,v);
+       
+    }
     vector<int> preorderTraversal(TreeNode* root) {
-        vector<int> answer;
-        TreeNode* curr = root;
-        TreeNode* last;
-        
-        while (curr != nullptr) {
-            // If there is no left child, go for the right child.
-            // Otherwise, find the last node in the left subtree. 
-            if (curr->left == nullptr) {
-                answer.push_back(curr->val);
-                curr = curr->right;
-            } else {
-                last = curr->left;
-                while (last->right != nullptr && last->right != curr) {
-                    last = last->right;
-                }
-                
-                // If the last node is not modified, we let 
-                // 'curr' be its right child. Otherwise, it means we 
-                // have finished visiting the entire left subtree.
-                if (last->right == nullptr) {
-                    answer.push_back(curr->val);
-                    last->right = curr;
-                    curr = curr->left;
-                } else {
-                    last->right = nullptr;
-                    curr = curr->right;
-                }
-            }
-        }
-
-        
-        return answer;
+        vector<int> v;
+        helper(root,v);
+        return v;
     }
 };
