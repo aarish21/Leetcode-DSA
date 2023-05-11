88cdc7312ab3fa4f33b289568feecd546bf40d57
aarish21 <aarish.jerry@gmail.com>
2023-05-12 03:36:56 +0530

    Time: 16 ms (78.67%), Space: 9.6 MB (97.63%) - LeetHub

--- 1035-uncrossed-lines/1035-uncrossed-lines.cpp
+++ 1035-uncrossed-lines/1035-uncrossed-lines.cpp
@@ -17,23 +17,19 @@ public:
         int n=num1.size();
         int m=num2.size();
 
-        vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
-        for(int i=0;i<=n;i++){
-            dp[i][0] = 0;
-        }
-        for(int i=0;i<=m;i++){
-            dp[0][i] = 0;
-        }
+        vector<int> prev(m+1,0),cur(m+1,0);
+       
 
         for(int ind1=1;ind1<=n;ind1++){
             for(int ind2=1;ind2<=m;ind2++){
                 if(num1[ind1-1]==num2[ind2-1])
-                    dp[ind1][ind2] = 1 + dp[ind1-1][ind2-1];
+                    cur[ind2] = 1 + prev[ind2-1];
                 else
-                    dp[ind1][ind2] = 0 + max(dp[ind1-1][ind2],dp[ind1][ind2-1]);
+                    cur[ind2] = 0 + max(prev[ind2],cur[ind2-1]);
             }
+            prev = cur;
         }
 
-        return dp[n][m];
+        return prev[m];
     }
 };
