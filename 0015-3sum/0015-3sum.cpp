1fd7968c0a4f8d0ceb2fdd45618e549207edadf9
aarish21 <aarish.jerry@gmail.com>
2023-05-23 23:37:24 +0530

    Time: 1171 ms (31.39%), Space: 30.9 MB (37.01%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -2,55 +2,55 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         sort(nums.begin() , nums.end()); 
-        map<int,int> hashMap;
+        map<int,int> map;
         vector<vector<int>> ans;
         for(int i=0;i<nums.size();i++){
-            hashMap[nums[i]]=i;
+            map[nums[i]]=i;
         }
-//         for(int x=0;x<nums.size();++x){
+        for(int x=0;x<nums.size();++x){
           
-//             if(nums[x] > 0){     
-//                 break;
-//             }
+            if(nums[x] > 0){     
+                break;
+            }
 
             
             
-//             for(int i=x+1;i<nums.size();++i){
-//                 int target = -(nums[x]+nums[i]);
-//                 vector<int> temp;
+            for(int i=x+1;i<nums.size();++i){
+                int target = -(nums[x]+nums[i]);
+                vector<int> temp;
                 
-//                 if(map.count(target-nums[i])>=1 && map.find(target)->second > i){
-//                     temp.push_back(nums[x]);
-//                     temp.push_back(nums[i]);
-//                     temp.push_back(target);
-//                     ans.push_back(temp);
-//                 }
-//                 i = map.find(nums[i])->second;
-//             }
-//             x = map.find(nums[x])->second; 
-           
-//         }
-//         return ans;
-        for(int i = 0 ; i < nums.size()  ; i++){    
-            if(nums[i] > 0){     
-                break;
-            }
-            for(int j = i + 1 ; j < nums.size()  ; j++){ 
-                
-                int required = -(nums[i] + nums[j]);  
-                
-                if(hashMap.count(required) && hashMap.find(required)->second > j){ 
-                    
-                    ans.push_back({nums[i] , nums[j] , required});
-                    
+                if(map.count(target)>=1 && map.find(target)->second > i){
+                    temp.push_back(nums[x]);
+                    temp.push_back(nums[i]);
+                    temp.push_back(target);
+                    ans.push_back(temp);
                 }
-                
-                j = hashMap.find(nums[j])->second; 
+                i = map.find(nums[i])->second;
             }
-            
-            i = hashMap.find(nums[i])->second;    
+            x = map.find(nums[x])->second; 
+           
         }
-        return ans;  
+        return ans;
+//         for(int i = 0 ; i < nums.size()  ; i++){    
+//             if(nums[i] > 0){     
+//                 break;
+//             }
+//             for(int j = i + 1 ; j < nums.size()  ; j++){ 
+                
+//                 int required = -(nums[i] + nums[j]);  
+                
+//                 if(hashMap.count(required) && hashMap.find(required)->second > j){ 
+                    
+//                     ans.push_back({nums[i] , nums[j] , required});
+                    
+//                 }
+                
+//                 j = hashMap.find(nums[j])->second; 
+//             }
+            
+//             i = hashMap.find(nums[i])->second;    
+//         }
+        // return ans;  
         
     }
 };
