847235c6f9ff6a98da8938b994a0648dd7d06196
aarish21 <aarish.jerry@gmail.com>
2023-08-14 12:31:47 +0530

    Time: 0 ms (100.00%), Space: 9.4 MB (21.19%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,44 +1,12 @@
 class Solution {
 public:
-    string longestCommonPrefix(vector<string>& strs) {
-//         string ans;
-//         if(strs.size() == 1){
-//             return strs[0];
-//         }
-//          int idx = INT_MAX;
-//         int len = strs[0].length();
-//         for(int i=0;i<strs.size()-1;i++){
-//             int y = strs[i+1].length();
-//             len = min(y,len);
-//             cout<<len<< " ";
-//             if(len == 1){
-//                if(strs[i][0]==strs[i+1][0]){
-//                    idx = 1;
-//                } else {
-//                    idx = 0;
-//                    break;
-//                }
-//             }
-//           for(int j=0;j<len;j++){
-//               if(strs[0][j] != strs[i+1][j]){
-//                   idx = min(j,idx);
-                  
-//               } 
-//           }
-//         }
-//         cout<<idx;
-//         if(idx == INT_MAX){
-//             idx = len;
-//         }
-//         return strs[0].substr(0,idx);
-        
-         if (strs.size() == 0) return "";
-    string prefix = strs[0];
-    for (int i = 1; i < strs.size(); i++)
-        while (strs[i].find(prefix) != 0) {
-            prefix = prefix.substr(0, prefix.length() - 1);
-            if (prefix.empty()) return "";
-        }        
-    return prefix;
+    string longestCommonPrefix(vector<string>& s) {
+        int ans = s[0].length(), n = s.size();
+        for(int i=1; i<n; i++){
+            int j = 0;
+            while(j<s[i].length() && s[i][j]==s[0][j])j++;
+            ans = min(ans, j);
+        }
+        return s[0].substr(0, ans);
     }
 };
