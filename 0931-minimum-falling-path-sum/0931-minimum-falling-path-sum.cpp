6c6bfdd5fa88808a766c70d786d26e3c2bdf0bbd
aarish21 <aarish.jerry@gmail.com>
2023-09-23 22:02:22 +0530

    Time: 12 ms (65.61%), Space: 11.1 MB (7.28%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -1,46 +1,31 @@
 class Solution {
 public:
-//     int minFallingPathSum(int i,int j,vector<vector<int>>& matrix,vector<vector<int>> &dp) {
-//         if(j<0 || j>=matrix[0].size()) return 1e9;
-//         if(i==0) return matrix[0][j];
-//         if(dp[i][j]!=-1) return dp[i][j];
-//         int up  = matrix[i][j] + minFallingPathSum(i-1,j,matrix,dp);
-//         int l =  matrix[i][j] + minFallingPathSum(i-1,j-1,matrix,dp);
-//         int r =  matrix[i][j] + minFallingPathSum(i-1,j+1,matrix,dp);
-        
-//         return dp[i][j]=min(up,min(l,r));
-        
-//     }
     int minFallingPathSum(vector<vector<int>>& matrix) {
-        int n = matrix.size(),m = matrix[0].size();
+        int n=matrix.size();
+        int ans = INT_MAX;
         
-        vector<vector<int>> dp(n,vector<int>(m,-1));
-        for(int j=0;j<m;j++){
-            dp[0][j] = matrix[0][j];
+        vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+        
+        for(int j=0;j<n;j++){
+            dp[n-1][j] = matrix[n-1][j];
         }
         
-        for(int i = 1;i<n;i++){
-            for(int j=0;j<m;j++){
-                int up=matrix[i][j]+dp[i-1][j],l=matrix[i][j],r=matrix[i][j];
-                
-                int leftDiagonal= matrix[i][j];
-            if(j>0) leftDiagonal += dp[i-1][j-1];
-            else leftDiagonal += 1e9;
-            
-            int rightDiagonal = matrix[i][j];
-            if(j<m-1) rightDiagonal += dp[i-1][j+1];
-            else rightDiagonal += 1e9;
-            
-            dp[i][j] = min(up, min(leftDiagonal,rightDiagonal));
+        for(int i=n-2;i>=0;i--){
+            for(int j=n-1;j>=0;j--){
+                int down2 = 1e9,down3 = 1e9;
+                int down1 = matrix[i][j]+dp[i+1][j];
+                if(j<n-1)
+                    down2 = matrix[i][j]+dp[i+1][j+1];
+                 if(j>0)
+                    down3 = matrix[i][j]+dp[i+1][j-1];
+                dp[i][j] = min(down1,min(down2,down3));
             }
         }
-        
-        int mini = INT_MAX;
-        for(int j=0; j<m;j++){
-            int ans = dp[n-1][j];
-            mini = min(mini,ans);
+        for(int j=0;j<n;j++){
+            ans = min(ans,dp[0][j]);
         }
-    
-        return mini;
+        
+        return ans;
+        
     }
 };
