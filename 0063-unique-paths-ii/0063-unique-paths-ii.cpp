293641b179ed37c7c592d6427943d1049732ac86
aarish21 <aarish.jerry@gmail.com>
2023-09-23 20:15:55 +0530

    Time: 5 ms (40.15%), Space: 8.2 MB (5.54%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,44 +1,20 @@
 class Solution {
 public:
-    int uniquePathsWithObstacles(int i,int j,vector<vector<int>>& obstacleGrid,vector<vector<int>>& dp) {
-        if(i>=0 && j>=0 && obstacleGrid[i][j]==1) return 0; 
-        if(i==0 && j == 0) return 1;
-        if(i<0 || j<0) return 0;
-        
-        if(dp[i][j]!=-1) return dp[i][j];
-        
-        int up = uniquePathsWithObstacles(i-1,j,obstacleGrid,dp);
-        int left = uniquePathsWithObstacles(i,j-1,obstacleGrid,dp);
-        return dp[i][j] = up+left;
+    int f(int i,int j,vector<vector<int>>& arr,vector<vector<int>> &dp){
+        if(i==0 && j==0) return 1;
+        if(dp[i][j]!=-1) return dp[i][j]; 
+        int down=0,right=0;
+        if(i>0 && arr[i][j]!=1)
+            down = f(i-1,j,arr,dp);
+        if(j>0 && arr[i][j]!=1)
+            right = f(i,j-1,arr,dp);
+        return dp[i][j] = down+right;
     }
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        if(obstacleGrid[0][0]==1) return 0;
         int n = obstacleGrid.size();
         int m = obstacleGrid[0].size();
-        vector<int> prev(m,0);
-    
-        for(int i=0;i<n;i++){
-             vector<int> cur(m,0);
-            for(int j=0;j<m;j++){
-                if(i>=0 && j>=0 && obstacleGrid[i][j]==1){
-                    cur[j]=0; continue;
-                }
-                if(i==0 && j==0){
-                    cur[j]=1;  continue;
-                }
-                
-                 
-                int up = 0, left = 0;
-                if(i>0) up = prev[j];
-                if(j>0) left = cur[j-1];
-                    
-                cur[j] = up+left;
-            
-            }
-            prev = cur;
-        }
-        
-        
-    return prev[m-1];
-     
+        vector<vector<int>> dp(n,vector<int>(m,-1));
+        return f(n-1,m-1,obstacleGrid,dp);
     }
 };
