c6a896175b530c9814356e06c56320aeee2b7cdd
aarish21 <aarish.jerry@gmail.com>
2023-09-28 09:37:25 +0530

    Time: 39 ms (20.31%), Space: 18.9 MB (24.15%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,17 +1,5 @@
 class Solution {
 public:
-     int f(int i, vector<int>& coins, int amount, vector<vector<int>> &dp){
-        if(amount==0) return 1;
-        if(i==0){
-            return 0;
-        }
-        if(dp[i][amount] != -1) dp[i][amount];
-        int take = 1e9;
-        if(coins[i]<=amount)
-            take = 1+f(i,coins,amount-coins[i],dp);
-        int notTake = f(i-1,coins,amount,dp);
-        return dp[i][amount] = min(take,notTake);
-    }
     int change(int amount, vector<int>& coins) {
        
         vector<vector<int>> dp(coins.size()+1,vector<int>(amount+1,0));
@@ -19,9 +7,11 @@ public:
         
         for(int i=1;i<=coins.size();i++){
             for(int j=1;j<=amount;j++){
-                dp[i][j] = dp[i-1][j];
+                int notTaken = dp[i-1][j];
+                int taken = 0;
                 if(coins[i-1]<=j)
-                    dp[i][j] += dp[i][j - coins[i - 1]] ;
+                    taken = dp[i][j-coins[i-1]];
+                dp[i][j] = notTaken + taken;
             }
         }
         return dp[coins.size()][amount];
