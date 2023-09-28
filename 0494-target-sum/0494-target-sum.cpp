1bfb9b9b4358608166523ff5b9ddc218f28660fd
aarish21 <aarish.jerry@gmail.com>
2023-09-28 09:07:39 +0530

    Time: 23 ms (55.28%), Space: 83.2 MB (5.96%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -3,14 +3,15 @@ public:
     int f(int i, vector<int>& nums, int target, vector<vector<int>> &dp) {
         if(i==-1 &&  target==0) return 1;
         if(i==-1) return 0;
-        // if(dp[i][target+1000]!=-1) return dp[i][target+1000];
+        if(dp[i][target+2000]!=-1) return dp[i][target+2000];
         int take1 = f(i-1,nums,+nums[i]+target,dp);
         int take2 = f(i-1,nums,-nums[i]+target,dp);
-        return take1+take2;
+        return dp[i][target+2000] = take1+take2;
     }
     int findTargetSumWays(vector<int>& nums, int target) {
-        vector<vector<int>> dp(nums.size(),vector<int>(2005,-1));
+        vector<vector<int>> dp(nums.size()+1,vector<int>(5005,-1));
         return f(nums.size()-1,nums,target, dp);
     }
+    
    
 };
