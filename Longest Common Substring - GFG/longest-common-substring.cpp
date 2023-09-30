df6186db53d4bf06722a1666d843fec6ecc59d3f
aarish21 <aarish.jerry@gmail.com>
2023-09-30 11:37:40 +0530

    Added solution - LeetHub

--- Longest Common Substring - GFG/longest-common-substring.cpp
+++ Longest Common Substring - GFG/longest-common-substring.cpp
@@ -6,24 +6,41 @@ using namespace std;
 class Solution{
     public:
   
-    int longestCommonSubstr (string S1, string S2, int n, int m)
-    {
-        // your code here
-        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
-        int ans = 0;
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=m;j++){
-                if(S1[i-1]==S2[j-1]){
-                    int val = 1 + dp[i-1][j-1];
-                    dp[i][j] = val;
-                    ans = max(ans,val);
-                }
-                else
-                    dp[i][j] = 0;
+    int f(int i, int j, string s1, string s2, vector<vector<int>>& dp) {
+    if (i == 0 || j == 0) return 0;
+
+    if (dp[i][j] != -1) {
+        return dp[i][j];
+    }
+
+    int ans = 0;
+    if (s1[i - 1] == s2[j - 1]) {
+        ans = 1 + f(i - 1, j - 1, s1, s2, dp);
+    }
+
+    dp[i][j] = ans;
+    return ans;
+}
+
+int longestCommonSubstr(string S1, string S2, int n, int m) {
+    vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
+
+    // Initialize the answer to store the length of the longest common substring.
+    int ans = 0;
+
+    // Call the recursive function for all possible pairs of substrings.
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= m; j++) {
+            if (S1[i - 1] == S2[j - 1]) {
+                int val = f(i, j, S1, S2, dp);
+                ans = max(ans, val);
             }
         }
-        return ans;
     }
+
+    return ans;
+}
+
 };
 
 
